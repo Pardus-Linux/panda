daca21fbfad58c11332460911d1b6e7f254fc3ea
Fatih Arslan <farslan@pardus.org.tr>
2010-12-23 13:07:44 +0000

    CLI for panda

---
+++ panda-cli.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+
+import panda
+
+def main(args):
+    p = panda.Panda()
+
+    if args.cur is not None:
+        status = p.update_grub_entries()
+        print "The driver currently used is: %s" % status
+
+    if args.up is not None:
+        status = p.update_grub_entries(args.up[0])
+        print status
+
+
+
+def argument():
+    '''Command line parser'''
+    parser = argparse.ArgumentParser(description='Pardus Alternative Driver Administration',
+                                     prog='panda',
+                                     usage='%(prog)s [options]')
+
+    parser.add_argument('-v', '--version',
+                         action='version',
+                         version='%(prog)s 0.0.1')
+
+    subparsers = parser.add_subparsers(help='Commands')
+    parser_cur = subparsers.add_parser('cur', help='Show currently used driver')
+    parser_cur.add_argument('cur',
+                            nargs='*',
+                            help="Show currently used driver")
+
+    parser_update = subparsers.add_parser('up', help='Update grub.conf')
+    parser_update.add_argument('up',
+                            nargs=1,
+                            help="Update grub.conf")
+
+    # We have to set them all to false, otherwise optional arguments are not
+    # passed to the args() namespace
+    parser.set_defaults(cur=None,
+                        up=None)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = argument()
+    sys.exit(main(args))
