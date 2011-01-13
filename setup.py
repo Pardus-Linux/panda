16c2fe19d4751da9ed87bef65796763fa4973027
Fatih Arslan <farslan@pardus.org.tr>
2011-01-13 06:54:11 +0000

    Increase release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name="panda",
-    version="0.1.2",
+    version="0.1.3",
     description="Python Modules for panda",
     license="GNU GPL2",
     url="http://www.pardus.org.tr/",
